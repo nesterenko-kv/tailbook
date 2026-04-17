c8aff006221ab49be90444dea395cdb16819cfef
Nesterenko Konstantin <nestquik@gmail.com>
2026-04-17 08:58:25 +0300

    fix: ports for ui

--- apps/admin-web/lib/env.ts
+++ apps/admin-web/lib/env.ts
@@ -1,4 +1,4 @@
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080";
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://localhost:5001";
 
 export const env = {
     apiBaseUrl
