cc5c210eab6fb2420eaedc3f8bfbe45426a50bed
Nesterenko Konstantin <nestquik@gmail.com>
2026-04-29 21:36:43 +0300

    fix: exclude codecoverage for EF migrations

---
+++ backend/src/Tailbook.BuildingBlocks/Migrations/MigrationsCoverageExclusions.cs
@@ -0,0 +1,88 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tailbook.BuildingBlocks.Migrations;
+
+[ExcludeFromCodeCoverage]
+public partial class Init
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class CustomerPetsFoundation
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class CatalogOffersFoundation
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class PricingDurationSnapshotsFoundation
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class StaffSchedulingFoundation
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class BookingRequests
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class VisitOperationsFoundation
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class ClientPortalMvp
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class AuditReportingNotificationsFoundation
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class BreedAllowedCoatTypes
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class BreedAllowedSizeCategories
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class GuestFlow
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class IdentityRefreshTokens
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class IdentityPasswordResetTokens
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class IdentityMfaFactors
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class NotificationFailureVisibility
+{
+}
+
+[ExcludeFromCodeCoverage]
+partial class AppDbContextModelSnapshot
+{
+}
