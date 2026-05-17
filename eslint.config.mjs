6fc46dd5374c306aac16fb4a6f1dd9847a47b6a9
Nesterenko Konstantin <nestquik@gmail.com>
2026-05-17 19:17:53 +0300

    test: add component tests and migration validation, fix vitest cleanup and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,5 @@
-import { defineConfig } from "eslint/config";
-import adminConfig from "./apps/admin-web/eslint.config.mjs";
-import clientConfig from "./apps/client-web/eslint.config.mjs";
-import groomerConfig from "./apps/groomer-web/eslint.config.mjs";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  ...adminConfig,
-  ...clientConfig,
-  ...groomerConfig,
+    globalIgnores(["**"])
 ]);
